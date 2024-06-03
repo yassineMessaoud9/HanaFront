990a8de043ed84d8046d3d0b67fe986b6154c7a0
AlaeddineBouabid <Alaeddine.Bouabid@esprit.tn>
2024-06-03 12:52:27 +0100

    add lien

--- src/app/service/admin.service.ts
+++ src/app/service/admin.service.ts
@@ -8,68 +8,68 @@ export class AdminService {
   constructor(private http:HttpClient) { }
 
   getGroups(){
-    return this.http.get('http://localhost:3005/groupe')
+    return this.http.get('https://hanaback.onrender.com/groupe')
   }
   //add groute
   addGroup(body:any){
-    return this.http.post('http://localhost:3005/groupe',body)
+    return this.http.post('https://hanaback.onrender.com/groupe',body)
   }
 
   getGroupbyId(id: any) {
-    return this.http.get('http://localhost:3005/groupe/'+id)
+    return this.http.get('https://hanaback.onrender.com/groupe/'+id)
   }
 
   updateGroup(id:any,body:any){
-    return this.http.put('http://localhost:3005/groupe/'+id,body)
+    return this.http.put('https://hanaback.onrender.com/groupe/'+id,body)
   }
 
   deleteGroup(id:any){
-    return this.http.delete('http://localhost:3005/groupe/'+id)
+    return this.http.delete('https://hanaback.onrender.com/groupe/'+id)
   }
 
   ////// Zone ///////
   createZone(body:any){
-    return this.http.post('http://localhost:3005/zone',body)
+    return this.http.post('https://hanaback.onrender.com/zone',body)
   }
 
   getZones(){
-    return this.http.get('http://localhost:3005/zone')
+    return this.http.get('https://hanaback.onrender.com/zone')
   }
 
   deleteZone(id:any){
-    return this.http.delete('http://localhost:3005/zone/'+id)
+    return this.http.delete('https://hanaback.onrender.com/zone/'+id)
   }
 
   updateZone(id:any,body:any){
-    return this.http.put('http://localhost:3005/zone/'+id,body)
+    return this.http.put('https://hanaback.onrender.com/zone/'+id,body)
   }
 
   getZoneById(id:any){
-    return this.http.get('http://localhost:3005/zone/'+id)
+    return this.http.get('https://hanaback.onrender.com/zone/'+id)
   }
 
 
   getListUsers(){
-    return this.http.get('http://localhost:3005/user')
+    return this.http.get('https://hanaback.onrender.com/user')
   }
 
   addUsers(body:any){
-    return this.http.post('http://localhost:3005/user/register',body)
+    return this.http.post('https://hanaback.onrender.com/user/register',body)
   }
 
   getUserById(id:any){
-    return this.http.get('http://localhost:3005/user/'+id)
+    return this.http.get('https://hanaback.onrender.com/user/'+id)
   }
 
   updateUser(id:any,body:any){
-    return this.http.put('http://localhost:3005/user/update/'+id,body)
+    return this.http.put('https://hanaback.onrender.com/user/update/'+id,body)
   }
   regeneratePassword(id:any){
-    return this.http.get('http://localhost:3005/user/regenerate-password/'+id)
+    return this.http.get('https://hanaback.onrender.com/user/regenerate-password/'+id)
   }
 
   deleteUsers(id:any){
-    return this.http.delete('http://localhost:3005/user/'+id)
+    return this.http.delete('https://hanaback.onrender.com/user/'+id)
   }
 
 }
--- src/environnement.ts
+++ src/environnement.ts
@@ -1,3 +1,3 @@
 export class Environnement {
-    static BASE_URL = 'http://localhost:3005';
+    static BASE_URL = 'https://hanaback.onrender.com';
 }
